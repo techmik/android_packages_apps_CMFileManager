5855bc1447ae4ff9b5d1c5d79725c9e77b701193
techmik <techmik67@gmail.com>
2013-11-04 01:54:48 -0500

    Slightly modified Samba Explorer  Initial commit  TODO: Add xml
    Change-Id: I7f8e87b23d6f11c0452d0ab7e1b17bf8dc81197a

---
+++ src/com/cyanogenmod/filemanager/PickHost.java
@@ -0,0 +1,59 @@
+package com.cyanogenmod.filemanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+import com.cyanogenmod.filemanager.R;
+
+public class PickHost extends Activity {
+        
+        @Override
+    public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                
+                setContentView(R.layout.pickhost);
+        
+                Button btn = (Button)findViewById(R.id.Button01);
+                btn.setOnClickListener(mPickHostClickListener);
+    }
+
+    private OnClickListener mPickHostClickListener = new OnClickListener()
+    {
+        public void onClick(View v)
+        {
+                EditText ip = (EditText)findViewById(R.id.EditText01);
+                String text = ip.getText().toString();
+                
+
+                        if (text.startsWith("\\\\")) {
+                                text = text.substring(2);
+                        }
+                if (text.startsWith("\\")) {
+                                text = text.substring(1);
+                }
+                if (text.startsWith("//")) {
+                                text = text.substring(2);
+                }
+                if (text.startsWith("/")) {
+                                text = text.substring(1);
+                }
+                if (text.endsWith("\\")) {
+                                text = text.substring(0, text.length()-1);
+                }
+                if (text.endsWith("/")) {
+                                text = text.substring(0, text.length()-1);
+                }
+                
+                text = "smb://"+text+"/";
+                
+                Intent intent = new Intent(v.getContext(), com.cyanogenmod.filemanager.SambaExplorer.class);
+                intent.putExtra("hostname", text);
+                startActivity(intent);  
+        }
+    };  
+}
---
+++ src/com/cyanogenmod/filemanager/SambaLogin.java
@@ -0,0 +1,44 @@
+package com.cyanogenmod.filemanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.cyanogenmod.filemanager.R;
+
+public class SambaLogin extends Activity {
+        public String share;
+
+        @Override
+    public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+
+                Intent i = getIntent();
+                share = i.getStringExtra("share");
+
+                setContentView(R.layout.login);
+
+                Button btn = (Button)findViewById(R.id.Button01);
+                btn.setOnClickListener(new OnClickListener()
+            {
+                public void onClick(View v)
+                {
+                        EditText domain = (EditText)findViewById(R.id.EditTextDomain);
+                        EditText username = (EditText)findViewById(R.id.EditTextUsername);
+                        EditText password = (EditText)findViewById(R.id.EditTextPassword);
+
+                        com.cyanogenmod.filemanager.DownloadService.ProvideLoginCredentials(domain.toString(), username.toString(), password.toString());
+
+                        Intent intent = new Intent();
+                        intent.setData(Uri.parse(share));
+                        startActivity(intent);  
+                }
+            });
+    }
+
+}
